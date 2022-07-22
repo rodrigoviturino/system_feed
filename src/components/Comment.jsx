2001823fb284d74ba12c3706a29ff1816d84ecfb
Rodrigo Viturino <viturino_souza@outlook.com>
2022-07-22 17:30:14 -0300

    Criando funcionalidade like dos comentarios

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -14,6 +14,7 @@ export function Comment({ content, onDeleteComment }){
   function handleLikeCount(){
     setLikeCount(likeCount + 1);
   }
+  
 
   return(
     <section className={styles.comment}>
